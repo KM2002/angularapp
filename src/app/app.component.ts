3f97e1304ed6a455c555ddc6b8e8aee14d49a103
k <@>
2022-09-27 10:12:35 +0530

    New commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
+interface Todolist{
+  title: string;
+  description: string;
+  date: Date;
+
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  todoItem: string='';
+
+  
+  todolist: Todolist[] = [];
+
+
+  addtodolist=()=>{
+    this.todolist.push({title:this.todoItem, description:this.tododescription, date: new Date()})
+  }
+
+  removelist=(myIndex:number)=>{
+    this.todolist.splice(myIndex,1)
+  }
+tododescription: any;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Todocomponent } from './Todo/todo.component';
+import { TodoAddComponent } from './Todo/todo-add/todo-add.component';
+import { TodoListComponent } from './Todo/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Todocomponent,
+    TodoAddComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
